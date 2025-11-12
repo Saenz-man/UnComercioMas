50407cdb3b54aaccc2673d13b1a8f89cd94bb3b9
Saenz-man <ing.saenzman@outlook.com>
2025-11-11 23:46:51 -0600

    Prueba de despliegue Deploy en Produccion

--- src/main.ts
+++ src/main.ts
@@ -80,7 +80,7 @@ async function bootstrap() {
 
   // --- CONFIGURACIÓN DE SWAGGER (CDN) ---
   const swaggerTitle = isProd
-    ? 'UnComercioMas API Web - Ganamos la batalla Por Gracias Yisus'
+    ? 'UnComercioMas API Web - Produccion'
     : 'UnComercioMas API Local - Entorno de Desarrollo';
 
   const swaggerDescription = isProd
